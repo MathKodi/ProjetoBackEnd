1c7936a2f4401e355ae03fdb4ae37660ac43818d
Matheus Kodi <matheuskodi2@gmail.com>
2023-11-24 20:07:48 -0300

    conectando a api com o banco de dados
    usando mongodb atlas e usando o dotenv para guardar informações do meu login ao banco.

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+
+const app = express()
+
+//rota publica
+app.get('/', (req,res) => {
+    res.status(200).json({msg: "Bem vindo ao API"})
+})
+
+//registrar
+app.post('/auth/registrar', async(req, res) => {
+    
+})
+
+const dbUser = process.env.DB_USER
+const dbPass = process.env.DB_PASS
+
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@cluster0.ucgj8xz.mongodb.net/`).then(() => {
+    app.listen(3000)
+    console.log('conectou ao banco')
+}).catch((err) => console.log(err))
+
