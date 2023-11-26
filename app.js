352094b4d1f042fd0ac41ee570682e367c614b5d
Matheus Kodi <matheuskodi2@gmail.com>
2023-11-25 23:14:31 -0300

    funcao verificar token, rota login e rota privada

--- app.js
+++ app.js
@@ -49,6 +49,72 @@ app.post('/auth/registrar', async(req, res) => {
 
 })
 
+//login
+app.post("/auth/login", async(req, res) => {
+    const {nome, senha} = req.body
+
+    if(!nome) {
+        return res.status(422).json({msg: 'erro -> digite um nome'})
+    }
+    if(!senha) {
+        return res.status(422).json({msg: 'erro -> digite uma senha'})
+    }
+    //verificar se usuario existe
+    const usuarioexistente = await Usuario.findOne({nome: nome})
+
+    if (!usuarioexistente) {
+        return res.status(422).json({msg: 'usuario nao encontrado'})
+    }
+    //verificar senha
+    if(senha != usuarioexistente.senha) {
+        return res.status(422).json({msg: 'senha invalida'})
+    }
+    try{
+        const secret = process.env.SECRET
+
+        const token = jwt.sign(
+            {
+                id: usuarioexistente._id
+            },
+            secret,
+        )
+        res.status(200).json({msg: 'Autenticacao realizada', token})
+    }
+    catch(error){
+        console.log(error)
+        res.status(500).json({msg: 'erro no servidor'})
+    }
+})
+
+//privado
+app.get("/usuario/:id",veriftoken, async (req, res) => {
+    const id = req.params.id
+    const user = await Usuario.findById(id, '-senha')
+    if(!user){
+        return res.status(404).json({msg:'usuario nao encontrado'})
+    }
+    res.status(200).json({user})
+
+    
+})
+
+function veriftoken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(" ")[1]
+    if(!token){
+        return res.status(401).json({msg:'Acesso negado'})
+    }
+    try{
+        const secret = process.env.SECRET
+        jwt.verify(token, secret)
+
+        next()
+
+    } catch(error){
+        res.status(400).json({msg: 'token invalido'})
+    }
+}
+
 const dbUser = process.env.DB_USER
 const dbPass = process.env.DB_PASS
 
