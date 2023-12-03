0e2eb5ba15e8414cf73c8b21d6644f1036485afb
MathKodi <matheuskodi2@gmail.com>
2023-12-03 20:52:41 -0300

    rota inicial do pokemon

--- routes/pokeAPI.js
+++ routes/pokeAPI.js
@@ -1,6 +1,13 @@
 const router = require('express').Router();
 const jwt = require('jsonwebtoken')
 
+const Pokemon = require('../models/Pokemon');
+const authhelper = require('../helpers/auth');
+
+router.get('/', (req,res) => {
+    res.status(200).json({msg: "rota p Pokemon, Treinador e Habilidades"})
+})
+
 
 
 module.exports = router
